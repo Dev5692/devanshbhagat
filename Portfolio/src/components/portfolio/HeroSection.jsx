4a256cc8071b1ab910e7c2bbdf6c717cc34815d8
devanshbhagat <bhagat.devansh1107@gmail.com>
2026-02-21 17:56:54 -0800

    Update HeroSection.jsx

--- Portfolio/src/components/portfolio/HeroSection.jsx
+++ Portfolio/src/components/portfolio/HeroSection.jsx
@@ -15,7 +15,7 @@ export default function HeroSection() {
         transition={{ delay: 0.2, duration: 0.8 }}
        className="absolute top-24 left-10"
       >
-       <img src="/logo.png" alt="Devansh Bhagat" className="h-16 w-auto" />
+       <img src="/logo.png" alt="Devansh Bhagat" className="h-20 w-auto" />
       </motion.div>
 
       <motion.div
